ad38ca8890d5ca58a1f9ce8af1c9e1e585714b2e
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-09 01:03:52 +0600

    design friend ship Schema

---
+++ models/friendShipModel.js
@@ -0,0 +1,15 @@
+const friendshipSchema = new mongoose.Schema({
+  requester: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending",
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: Date,
+});
+
+const Friendship = mongoose.model("Friendship", friendshipSchema);
+
+module.exports = Friendship;
