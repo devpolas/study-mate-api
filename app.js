e60d9b5de56038c181a64dcf0f12690fd6efe30f
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-13 16:18:47 +0600

    fix cors issue

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: [("Content-Type", "Authorization")],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Content-Range",
+      "X-Content-Range",
+    ],
     credentials: true,
   })
 );
