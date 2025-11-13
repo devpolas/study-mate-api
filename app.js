35301b5a49184c8f9f5b0b10e55943f9a1612829
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-13 19:14:47 +0600

    full optimise cors for with creadentials

--- app.js
+++ app.js
@@ -14,9 +14,24 @@ const app = express();
 app.use(cookieParser());
 // use json form data parsing middleware
 app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+"https://study-mate1.netlify.app",
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://study-mate1.netlify.app"],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman or curl)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
